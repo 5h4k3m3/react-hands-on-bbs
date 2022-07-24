f514149ae4f0b1b52e381317ecaf15aef4c90674
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-24 17:30:17 +0900

    ✨ impl post article

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../firebase";
 import "./styles/CreatePost.css";
+import { useNavigate } from "react-router-dom";
 
 export const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
+  const navigate = useNavigate();
+
+  const createPost = async () => {
+    await addDoc(collection(db, "posts"), {
+      title: title,
+      postText: postText,
+      author: {
+        username: auth.currentUser.displayName,
+        id: auth.currentUser.uid,
+      },
+    });
+    navigate("/");
+  };
+
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>Post</h1>
         <div className="inputPost">
           <div>Title</div>
-          <input type="text" placeholder="post title" />
+          <input
+            type="text"
+            placeholder="post title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="inputPost">
           <div>Article</div>
-          <textarea placeholder="post article"></textarea>
+          <textarea
+            placeholder="post article"
+            onChange={(e) => setPostText(e.target.value)}
+          ></textarea>
         </div>
-        <button className="postButton">Post</button>
+        <button className="postButton" onClick={createPost}>
+          Post
+        </button>
       </div>
     </div>
   );
