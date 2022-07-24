14e9712112d5688ce5a02a9e9a4b4dad51c0c55d
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-24 15:34:54 +0900

    ✨ init firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+import { firebaseConfig } from "../resources/firebaseConfig";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
