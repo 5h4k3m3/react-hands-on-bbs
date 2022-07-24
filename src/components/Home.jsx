ea31bc895bd97675095a1e701a7b223f3b037d9e
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-24 17:42:45 +0900

    ✨ impl home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import "./styles/Home.css";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>Title</h1>
+        </div>
+        <div className="postTextContainer">Article</div>
+        <div className="nameAndDeleteButton">
+          <h3>User</h3>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
 };
