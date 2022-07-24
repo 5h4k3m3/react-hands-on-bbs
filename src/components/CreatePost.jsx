2795242e6c44e924394b341077a500ee69cb77c4
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-24 17:17:27 +0900

    ✨ impl post page css

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import "./styles/CreatePost.css";
 
 export const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>Post</h1>
+        <div className="inputPost">
+          <div>Title</div>
+          <input type="text" placeholder="post title" />
+        </div>
+        <div className="inputPost">
+          <div>Article</div>
+          <textarea placeholder="post article"></textarea>
+        </div>
+        <button className="postButton">Post</button>
+      </div>
+    </div>
+  );
 };
