5283b45d9ef853d7cb6cba04579411d4d77ba127
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-24 17:55:35 +0900

    ✨ impl post article and show content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { collection, doc, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 import "./styles/Home.css";
 
 export const Home = () => {
+  const [postList, setPostList] = useState([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>Title</h1>
+      {postList.map((post) => (
+        <div className="postContents" key={post.id}>
+          <div className="postHeader">
+            <h1>{post.title}</h1>
+          </div>
+          <div className="postTextContainer">{post.postText}</div>
+          <div className="nameAndDeleteButton">
+            <h3>{post.author.username}</h3>
+            <button>Delete</button>
+          </div>
         </div>
-        <div className="postTextContainer">Article</div>
-        <div className="nameAndDeleteButton">
-          <h3>User</h3>
-          <button>Delete</button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
